51ef4ebb08514b3dc44a975ac9bd58b4e19b6d3c
Rafael Cruz <ralph1786@gmail.com>
2018-11-14 19:47:40 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -10,3 +10,23 @@ def first_element(array)
   return array[0]
 end
 
+def third_element(array)
+  return array[2]
+end
+
+def last_element(array)
+  return array[-1]
+end
+
+def first_element_with_array_methods(array)
+  array.unshift()
+end
+
+def last_element_with_array_methods(array)
+  array.pop()
+end
+
+def length_of_array(array)
+  array.length
+end
+  
